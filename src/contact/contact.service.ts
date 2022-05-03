db202ec17f042e1351fa53247d57a273cc1fe5bb
moussa <moussa@Moussas-MacBook-Pro.local>
2022-05-03 14:45:37 +0200

    first commit

---
+++ src/contact/contact.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Contact } from '../entities/contact.entity';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+
+@Injectable()
+export class ContactService {
+  constructor(
+    @InjectRepository(Contact) private contactRepository: Repository<Contact>,
+  ) {}
+
+  async create(contact: Contact): Promise<Contact> {
+    return await this.contactRepository.save(contact);
+  }
+
+  async readAll(): Promise<Contact[]> {
+    return await this.contactRepository.find();
+  }
+
+  async update(contact: Contact): Promise<UpdateResult> {
+    return await this.contactRepository.update(contact.id, contact);
+  }
+
+  async delete(id): Promise<DeleteResult> {
+    return await this.contactRepository.delete(id);
+  }
+}
---
+++ src/contacts/contacts.controller.ts
@@ -0,0 +1,37 @@
+import { ContactService } from '../contact/contact.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { Contact } from '../entities/contact.entity';
+
+@Controller('api/contacts')
+export class ContactsController {
+  constructor(private contactService: ContactService) {}
+
+  @Get()
+  read(): Promise<Contact[]> {
+    return this.contactService.readAll();
+  }
+
+  @Post('create')
+  async create(@Body() contact: Contact): Promise<any> {
+    return this.contactService.create(contact);
+  }
+
+  @Put(':id/update')
+  async update(@Param('id') id, @Body() contact: Contact): Promise<any> {
+    contact.id = Number(id);
+    return this.contactService.update(contact);
+  }
+
+  @Delete(':id/delete')
+  async delete(@Param('id') id): Promise<any> {
+    return this.contactService.delete(id);
+  }
+}
