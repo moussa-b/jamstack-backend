fa946e0d150e585fa72f5e8c7bee60c75f27ba10
moussa <moussa@bdmz.fr>
2022-05-04 15:02:28 +0200

    test build auto

--- src/contact/contact.service.ts
+++ src/contact/contact.service.ts
@@ -15,13 +15,22 @@ export class ContactService {
         city: 'Rogers',
       },
       {
-        id: 5,
+        id: 2,
         name: 'CL 002',
         title: 'Manager',
         email: 'cl002@yopmail.com',
         phone: '0782880000',
         address: '123 Rue du lac',
         city: 'Paris',
+      },
+      {
+        id: 3,
+        name: 'CL 003',
+        title: 'Director',
+        email: 'cl003@yopmail.com',
+        phone: '0711110000',
+        address: '123 Rue des champs',
+        city: 'Lille',
       },
     ];
   }
