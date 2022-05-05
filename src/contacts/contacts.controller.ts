fd0190054f3f276e529af0ff26bb96b4d4418bd2
moussa <moussa@bdmz.fr>
2022-05-05 11:05:16 +0200

    Add swagger

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -9,28 +9,38 @@ import {
   Param,
 } from '@nestjs/common';
 import { Contact } from '../entities/contact.entity';
+import { ApiBody, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('contacts')
 @Controller('api/contacts')
 export class ContactsController {
   constructor(private contactService: ContactService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Return list of all contacts' })
   read(): Promise<Contact[]> {
     return this.contactService.readAll();
   }
 
   @Post('create')
+  @ApiOperation({ summary: 'Create a contact' })
+  @ApiBody({ description: 'Contact object in json format' })
   async create(@Body() contact: Contact): Promise<any> {
     return this.contactService.create(contact);
   }
 
   @Put(':id/update')
+  @ApiOperation({ summary: 'Update a contact' })
+  @ApiBody({ description: 'Contact object in json format' })
+  @ApiParam({ name: 'id', description: 'Unique id of the contact to update', type: Number })
   async update(@Param('id') id, @Body() contact: Contact): Promise<any> {
     contact.id = Number(id);
     return this.contactService.update(contact);
   }
 
   @Delete(':id/delete')
+  @ApiOperation({ summary: 'Delete a contact' })
+  @ApiParam({ name: 'id', description: 'Unique id of the contact to delete', type: Number })
   async delete(@Param('id') id): Promise<any> {
     return this.contactService.delete(id);
   }
--- src/entities/contact.entity.ts
+++ src/entities/contact.entity.ts
@@ -1,25 +1,33 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Contact {
+  @ApiProperty({ description: 'Unique id of the contact', type: Number })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ description: 'Name of the contact', type: String })
   @Column()
   name: string;
 
+  @ApiProperty({ description: 'Title of the contact', type: String })
   @Column()
   title: string;
 
+  @ApiProperty({ description: 'Email of the contact', type: String })
   @Column()
   email: string;
 
+  @ApiProperty({ description: 'Phone of the contact', type: String })
   @Column()
   phone: string;
 
+  @ApiProperty({ description: 'Address of the contact', type: String })
   @Column()
   address: string;
 
+  @ApiProperty({ description: 'City of the contact', type: String })
   @Column()
   city: string;
 }
