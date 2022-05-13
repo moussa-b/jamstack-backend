cd9a886ed9b1a3e7f4f54fe0df155086a0fae68b
moussa <moussa@bdmz.fr>
2022-05-13 10:43:08 +0200

    change open API documentation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @ApiOperation({ summary: 'Default app controller' })
+  @ApiOperation({ summary: 'Default application controller' })
   getHello(): string {
     return this.appService.getHello();
   }
